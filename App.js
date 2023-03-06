9e35a1766351f97b59a90c6ada4c8c5bd17c42b1
Gaurav Kumawat <gauravkumawat19earcs047@gmail.com>
2023-03-06 17:01:28 +0530

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
+    <>
+    <ImageBackground source={require("./Background.png")} style={{height:"100%"}}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ alignSelf:"center", marginTop:119}}>
+      <Text style={[styles.head]} > DigiDost </Text>
+    </View>
+      <Image source={require("./Backk.png")} style={[styles.stretch,{marginTop:70,alignSelf:"center"}]} />
+      
+      <Text style={[styles.titleText,{marginTop:35,alignSelf:"center"}]}>"Your Digital Friend"</Text>
       <StatusBar style="auto" />
     </View>
+    </ImageBackground>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // flex: 1,
+    // backgroundColor: '#4CC8D9',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  stretch: {
+    width: 363,
+    height: 238,
+    resizeMode: 'stretch',
+  },
+  baseText: {
+    // fontFamily: 'Cochin',
+  },
+  titleText: {
+    // fontFamily: 'Cochin',
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#110D59',
+  },
+
+  head: {
+    fontSize: 64,
+    color: '#110D59',
+    // fontFamily: "Abril Fatface"
+    // flex: 1,
+    // backgroundColor: '#4CC8D9',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  
   },
 });
